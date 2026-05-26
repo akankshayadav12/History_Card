1c8807f5a40f41e6e92cf6e4e8267c04917eade9
akankshayadav12 <akankshay1222@gmail.com>
2026-05-26 18:25:57 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,7 +3,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL:import.meta.env.VITE_API_URL || "http://localhost:5000",
+  // Add /api to the end of both the environment variable and the fallback string
+  baseURL: import.meta.env.VITE_API_URL 
+    ? `${import.meta.env.VITE_API_URL}/api` 
+    : "http://localhost:5000/api",
   withCredentials: true,
 });
 
